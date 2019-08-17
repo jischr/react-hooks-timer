c9be6d2840b0a81f0dfae16828602e5401899d0f
Jennifer Winer <jeniwiner@gmail.com>
2019-08-16 19:23:30 -0600

    switch state of state vars

--- src/Timer.js
+++ src/Timer.js
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
 
 function Timer() {
+    let [ seconds, setSeconds ] = useState(0);
+    let [ isActive, setIsActive ] = useState(false);
+
+    function toggle() {
+        setIsActive(!isActive);
+    }
+
+    function reset() {
+        setSeconds(0);
+        setIsActive(false);
+    }
 
     return (
         <div>
-            <h1>0s</h1>
-            <button>Start</button>
-            <button>Reset</button>
+            <h1>{seconds}s</h1>
+            <button onClick={toggle}>
+                { isActive ? 
+                    'Pause' : 'Start'
+                }
+            </button>
+            <button onClick={reset}>Reset</button>
         </div>
     );
 }
