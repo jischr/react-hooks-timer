61d4f2e0de5cca90dc252ae42761b93ec4a11f16
Jennifer Winer <jeniwiner@gmail.com>
2019-08-16 19:43:13 -0600

    timer actually timing

--- src/Timer.js
+++ src/Timer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Timer() {
     let [ seconds, setSeconds ] = useState(0);
@@ -12,6 +12,21 @@ function Timer() {
         setSeconds(0);
         setIsActive(false);
     }
+
+    useEffect(() => {
+        let interval = null;
+
+        if (isActive) {
+            interval = setInterval(() => {
+                setSeconds(seconds => seconds + 1);
+            }, 1000);
+        } else if (!isActive && seconds !== 0) {
+            clearInterval(interval);
+        }
+
+        return () => clearInterval(interval);
+
+    }, [isActive, seconds])
 
     return (
         <div>
